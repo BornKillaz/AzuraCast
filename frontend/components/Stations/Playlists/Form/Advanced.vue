8ac852922de53e032e5720edb7b238c5bf99972d
BornKillaz <10302997+BornKillaz@users.noreply.github.com>
2026-08-25 12:13:59 +0200

    feat: expose exact scheduled start in playlist UI

--- frontend/components/Stations/Playlists/Form/Advanced.vue
+++ frontend/components/Stations/Playlists/Form/Advanced.vue
@@ -38,6 +38,10 @@ const backendOptions = [
         text: $gettext("Interrupt other songs to play at scheduled time."),
     },
     {
+        value: "exact_start",
+        text: $gettext("Force exact start at scheduled time (interrupt AutoDJ, but not live DJs)."),
+    },
+    {
         value: "single_track",
         text: $gettext("Only play one track at scheduled time."),
     },
